03ca12ffed700725afd3c3991cc1dc1bc74f6908
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-03-20 15:20:46 -0300

    user_type

---
+++ src/main/java/com/yanvelasco/rasmooplus/model/UserType.java
@@ -0,0 +1,28 @@
+package com.yanvelasco.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_type")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_type_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+}
